a552005ac9f5412dc771409ee77be189453241e2
Andrew Arrow <oneone@gmail.com>
2022-09-07 11:44:35 -0700

    adding google lib

--- address/address.go
+++ address/address.go
@@ -1,11 +1,21 @@
 package address
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"strings"
+
+	"googlemaps.github.io/maps"
 )
 
 func Process(body string) {
+	c, err := maps.NewClient(maps.WithAPIKey("Insert-API-Key-Here"))
+	if err != nil {
+		log.Fatalf("fatal error: %s", err)
+		return
+	}
+
 	lines := strings.Split(body, "\n")
 	for _, line := range lines {
 		trimmedLine := strings.TrimSpace(line)
@@ -13,5 +23,19 @@ func Process(body string) {
 			continue
 		}
 		fmt.Println(trimmedLine)
+		GeocodeAdress(c, trimmedLine)
 	}
+}
+
+func GeocodeAdress(c *maps.Client, address string) {
+	r := &maps.GeocodingRequest{
+		Address: address,
+	}
+
+	res, err := c.Geocode(context.Background(), r)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(res[0].Geometry.Location.Lat, res[0].Geometry.Location.Lng)
 }
